e5e6e8aaf11f396f7f9cf82469c33b2f5a051af7
Stambolidi Ivan <aduanite1024bit@mail.ru>
2023-12-09 20:20:41 +0300

    2

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-let formNode = document.querySelector('.form');
-let VoitNode = document.querySelector('#Voit');
+const dialog = document.getElementById('myDialog')
+const dialogOpener = document.querySelector('.openDialogBtn')
+const dialogCloser = document.querySelector('.closeDialogBtn')
 
-VoitNode.addEventListener('click', () => {
-  formNode.classList.add('open');
-}); 
+function openModalAndLockScroll() {
+  dialog.showModal()
+  document.body.classList.add('scroll-lock')
+}
 
-console.log("Привет")
+function returnScroll() {
+  document.body.classList.remove('scroll-lock')
+}
+
+function close() {
+  dialog.close()
+  returnScroll()
+}
+
+dialogOpener.addEventListener('click', openModalAndLockScroll)
+dialogCloser.addEventListener('click', (event) => {
+  event.stopPropagation()
+  close()
+})
+
+
+function closeOnBackDropClick({ currentTarget, target }) {
+  const dialog = currentTarget
+  const isClickedOnBackDrop = target === dialog
+  if (isClickedOnBackDrop) {
+    dialog.close()
+    returnScroll()
+  }
+}
+
+dialog.addEventListener('click', closeOnBackDropClick)
+
+
+console.log("fddf")
