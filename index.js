9cd6a26edb147a5ca821db3c7dd5e5867e76ea79
Stambolidi Ivan <aduanite1024bit@mail.ru>
2023-12-09 17:18:43 +0300

    1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
- 
+let formNode = document.querySelector('.form');
+let VoitNode = document.querySelector('#Voit');
+
+VoitNode.addEventListener('click', () => {
+  formNode.classList.add('open');
+}); 
+
+console.log("Привет")
